166a65b536adf5c22550c3166db6931a85dadb9c
tarena <tarena@tarenadeimac-15.local>
2016-06-16 11:22:30 +0800

    commit code

---
+++ TestTest/TestTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestTest
+//
+//  Created by tarena on 16/6/16.
+//  Copyright © 2016年 tarena. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"这是要提交到服务器代码...");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
